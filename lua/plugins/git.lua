8be3b7a32e7170ed36361f85ccf4aa5f02578562
Friday G <godswill.friday@parcellab.com>
2024-05-13 21:43:29 +0200

    Add bbq, git gutter commands

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -4,6 +4,17 @@ return {
 		"airblade/vim-gitgutter",
 		config = function()
 			vim.g.gitgutter_map_keys = 0
+			vim.keymap.set("n", "<leader>gh", "<cmd>GitGutterLineHighlightsToggle<CR>", {
+				desc = "Toggle GitGutter highlights",
+			})
+			vim.keymap.set("n", "<leader>gj", "<cmd>GitGutterNextHunk<CR>", { desc = "Jump to next GitGutter hunk" })
+			vim.keymap.set(
+				"n",
+				"<leader>gk",
+				"<cmd>GitGutterPrevHunk<CR>",
+				{ desc = "Jump to previous GitGutter hunk" }
+			)
+			vim.keymap.set("n", "<leader>gx", "<cmd>GitGutterUndoHunk<CR>", { desc = "Undo GitGutter hunk" })
 		end,
 	},
 	{
@@ -19,7 +30,7 @@ return {
 			"nvim-lua/plenary.nvim",
 		},
 		keys = {
-			{ "<leader>git", "<cmd>LazyGit<CR>", desc = "Toggle Lazy Git" },
+			{ "<leader>gt", "<cmd>LazyGit<CR>", desc = "Toggle Lazy Git" },
 		},
 		config = function()
 			require("telescope").load_extension("lazygit")
--- lua/plugins/lsp/outline.lua
+++ lua/plugins/lsp/outline.lua
@@ -1,7 +1,6 @@
 return {
 	"hedyhli/outline.nvim",
 	config = function()
-		-- Example mapping to toggle outline
 		vim.keymap.set("n", "<leader>o", "<cmd>Outline<CR>", { desc = "Toggle Outline" })
 
 		require("outline").setup({
